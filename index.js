96a8cb0de756bf372acb63b40cc4b63f6f29c8e1
Haim Guel Quiroz <hgq_2000@hotmail.com>
2023-01-13 13:07:53 -0600

    Agregar boton copiar

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 let btnEncriptar = document.querySelector(".btn-encriptar");
 
+let btn_Copiar  = document.querySelector(".btnCopiar");
+
+btn_Copiar.onclick = () => {
+    const text = document.querySelector(".AreaCodificar");
+    text.select();
+    document.execCommand("copy");
+    alert("Elemento copiado")
+}
 
 btnEncriptar.onclick = () => {
     let texto = document.querySelector(".text").value;
@@ -8,21 +16,12 @@ btnEncriptar.onclick = () => {
     cadenas.forEach(element => {
         for(let i = 0; i <= element.length - 1; i++){
             result = result + codificar(element.charAt(i))
-            // codificar(element.charAt(i))
-            // console.log(element.charAt(i));
-            // const newstr = element
-            // .replace(/a/i, "ai")
-            // .replace(/e/i, "enter")
-            // .replace(/i/i, "imes")
-            // .replace(/o/i, "ober")
-            // .replace(/u/i, "ufat");
         }
         result = result + " "
     });
     document.querySelector(".container__message-no__message").style.display = "none"
     document.querySelector(".container__message-text").style.display = "flex"
     document.querySelector(".AreaCodificar").value = result
-    console.log(result);
 }
 
 function codificar(letra){
